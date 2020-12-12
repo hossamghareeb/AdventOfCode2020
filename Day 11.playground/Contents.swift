c495aa152a8f7ce305a574bff8d37907de8ad761
Hossam Ghareeb <hossam.ghareb@gmail.com>
2020-12-12 09:38:31 +0100

    Day 11

--- Day 11.playground/Contents.swift
+++ Day 11.playground/Contents.swift
@@ -6,12 +6,63 @@ extension String {
 }
 
 extension Array where Element == [Character] {
+    
     func emptyAdjacentCount(i: Int, j: Int) -> Int {
         return adjacentCount(char: "L", i: i, j: j)
     }
     
     func occupiedAdjacentCount(i: Int, j: Int) -> Int {
         return adjacentCount(char: "#", i: i, j: j)
+    }
+    
+    func occupiedDirectionalCount(i: Int, j: Int) -> Int {
+        var count = 0
+        let C = self[0].count
+        func check(c: Character) -> Bool {
+            if c == "." { return false }
+            else {
+                if c == "#" { count += 1 }
+                return true
+            }
+        }
+        for u in (0...i - 1).reversed() { if check(c: self[u][j]) { break } }
+        for d in (i + 1..<self.count) { if check(c: self[d][j]) { break } }
+        for l in (0...j - 1).reversed() { if check(c: self[i][l]) { break } }
+        for r in (j + 1..<C) { if check(c: self[i][r]) { break } }
+        
+        var r = i - 1
+        var c = j - 1
+        while r >= 0 && c >= 0 {
+            if check(c: self[r][c]) { break }
+            r -= 1
+            c -= 1
+        }
+        
+        r = i - 1
+        c = j + 1
+        while r >= 0 && c < C {
+            if check(c: self[r][c]) { break }
+            r -= 1
+            c += 1
+        }
+        
+        r = i + 1
+        c = j - 1
+        while r < self.count && c >= 0 {
+            if check(c: self[r][c]) { break }
+            r += 1
+            c -= 1
+        }
+        
+        r = i + 1
+        c = j + 1
+        while r < self.count && c < C {
+            if check(c: self[r][c]) { break }
+            r += 1
+            c += 1
+        }
+        
+        return count
     }
     
     private func adjacentCount(char: Character, i: Int, j: Int) -> Int {
@@ -97,10 +148,45 @@ func round2(grid: inout [[Character]]) -> Bool {
     return changed
 }
 
+func round1_2(grid: inout [[Character]]) -> Bool {
+    var copy = grid
+    var changed = false
+    for r in 1..<(grid.count - 1) {
+        for c in 1..<(grid[0].count - 1)  {
+            guard grid.isFloor(i: r, j: c) == false else { continue }
+            if grid.isEmpty(i: r, j: c) && grid.occupiedDirectionalCount(i: r, j: c) == 0 {
+                changed = true
+                copy[r][c] = "#"
+            }
+        }
+    }
+//    copy.log()
+    grid = copy
+    return changed
+}
+
+func round2_2(grid: inout [[Character]]) -> Bool {
+    var copy = grid
+    var changed = false
+    for r in 1..<(grid.count - 1)  {
+        for c in 1..<(grid[0].count - 1)  {
+            guard grid.isFloor(i: r, j: c) == false else { continue }
+            if grid.isOccupied(i: r, j: c) && grid.occupiedDirectionalCount(i: r, j: c) >= 5 {
+                changed = true
+                copy[r][c] = "L"
+            }
+        }
+    }
+//    copy.log()
+    grid = copy
+    return changed
+}
+
+
 //round1(grid: &grid)
 //round2(grid: &grid)
 
-while round1(grid: &grid) && round2(grid: &grid) {
+while round1_2(grid: &grid) && round2_2(grid: &grid) {
     print("Apply!")
 }
 
